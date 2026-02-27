a9e42de2f4bb60e243ab534d6289750d186c0137
Vladimir Upirov <4648606+Valodya@users.noreply.github.com>
2026-02-27 12:39:32 +0200

    - fix some dev modules

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,36 @@
-'use strict';
+'use strict'
 
 const webpack = require('webpack')
 const path = require('path')
 const TerserPlugin = require('terser-webpack-plugin')
+const pkg = require('./package.json')
 
 const isProd = process.env.NODE_ENV === 'production'
+
+const banner = `********************************************************************************************************************
+ *  Backendless RT Client for JavaScript. Version: ${ pkg.version }
+ *
+ *  Copyright 2012-${ new Date().getFullYear() } BACKENDLESS.COM. All Rights Reserved.
+ *
+ *  NOTICE: All information contained herein is, and remains the property of Backendless.com and its suppliers,
+ *  if any. The intellectual and technical concepts contained herein are proprietary to Backendless.com and its
+ *  suppliers and may be covered by U.S. and Foreign Patents, patents in process, and are protected by trade secret
+ *  or copyright law. Dissemination of this information or reproduction of this material is strictly forbidden
+ *  unless prior written permission is obtained from Backendless.com.
+ * ********************************************************************************************************************`
 
 module.exports = {
   devtool: 'source-map',
 
   target: 'web',
 
-  entry : './src/index.js',
+  entry: './src/index.js',
 
   output: {
     path         : path.resolve(__dirname, 'dist'),
     filename     : isProd ? 'backendless-rt-client.min.js' : 'backendless-rt-client.js',
     library      : 'BackendlessRTClient',
-    libraryTarget: 'umd'
+    libraryTarget: 'umd',
   },
 
   module: {
@@ -27,9 +40,9 @@ module.exports = {
       {
         test   : /\.js$/,
         exclude: /node_modules/,
-        loader : 'babel-loader'
-      }
-    ]
+        loader : 'babel-loader',
+      },
+    ],
   },
 
   optimization: {
@@ -43,7 +56,8 @@ module.exports = {
   },
 
   plugins: [
-    new webpack.NormalModuleReplacementPlugin(/socket\.io-parser/, __dirname + '/src/socket-parser')
-  ]
+    new webpack.NormalModuleReplacementPlugin(/socket\.io-parser/, __dirname + '/src/socket-parser'),
+    new webpack.BannerPlugin({ banner }),
+  ],
 }
 
